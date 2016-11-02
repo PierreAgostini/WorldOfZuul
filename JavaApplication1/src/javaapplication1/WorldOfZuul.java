9f053efbc14d8ec327cff2d3feada7279bb1de80
Wassym <Wassim.Kerkri@gmail.com>
2016-11-02 18:08:38 +0100

    Update and rename JavaApplication1.java to WorldOfZuul.java

--- JavaApplication1/src/javaapplication1/WorldOfZuul.java
+++ JavaApplication1/src/javaapplication1/WorldOfZuul.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication1;
+package WorldOfZuul;
 
 /**
  * Blablac !!
  * @author Group 5
  */
-public class JavaApplication1 {
+public class Test {
 
     /**
      * @param args the command line arguments
